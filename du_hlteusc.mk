6d87fb45dc23fd8fc42da091a7085623eb9ae9a1
moepda <moepda@gmail.com>
2014-12-22 00:22:21 +0000

    lets get dirty...unicorns

--- du_hlteusc.mk
+++ du_hlteusc.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/hlteusc/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_hlteusc
+PRODUCT_NAME := du_hlteusc
 PRODUCT_DEVICE := hlteusc
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_hlteusc-eng
+add_lunch_combo du_hlteusc-eng
